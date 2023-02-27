c7b1bb2f41607690c11fae392e66b7edb22e6c0b
ACRamirez1 <caramirez711@gmail.com>
2023-02-27 16:01:28 -0600

    2nd attempt and still a failure

--- main.js
+++ main.js
@@ -8,26 +8,54 @@ const rl = readline.createInterface({
 });
 
 
+    
+
+
+
+function rampNumber(num){
+
+if(num<0 || num>99999){
+        return;
+    }
+
+  var enterNumber = num.toString().split("").map(Number);
+  for (let i=0; i<enterNumber.length; i++)
+  { console.log(enterNumber[i])
+    if (enterNumber[i] < enterNumber[i-1])
+    {
+      return false;
+    }
+  }
+  return true;
+  
+}
+
+
+// console.log(rampNumber(11234));
+// console.log(rampNumber(99990));
+
 
 function numOfRampsBelow(num) {
-    let numArray = Array.from(Array(num).keys());
+    // let numArray = Array.from(Array(num).keys());
     let counter = 0;
-    let bigNumArr = [];
-
-    numArray.forEach(num => {
-        bigNumArr.push(num.toString().split(""))
-
-    })
-
+    // let bigNumArr = [];
+    let bigNum = num
     // console.log(bigNumArr)
 
-    for (let i = 0; i < bigNumArr.length; i++) {
-        for (let j = 0; j < bigNumArr[i].length; j++) {
-            if (bigNumArr[i][j] >= bigNumArr[i][j-1]) {
-                counter++
-            }
+    for (let i = 0; i < bigNum; i++) {
+        console.log(bigNum[i])
+        if(rampNumber(bigNum)) {
+            counter++
+        
         }
+        bigNum = bigNum - 1;
+        // for (let j = 0; j < rampNumber[i].length; j++) {
+        //     if (rampNumber[i][j] >= rampNumber[i][j-1]) {
+        //         counter++
+        //     }
+        // }
     }
+
 
 console.log( `${counter} total ramp numbers are less than ${num}`)
 
