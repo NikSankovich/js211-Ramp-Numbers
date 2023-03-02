8075bf405a2496b54f8aafa228f540a8dc098be2
NikSankovich <nik.sankovich@gmail.com>
2023-03-02 14:17:33 -0600

    first

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 
+const { log } = require('console');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,79 +9,101 @@ const rl = readline.createInterface({
 });
 
 
-    
 
 
 
-function rampNumber(num){
 
-if(num<0 || num>99999){
-        return;
-    }
-
-  var enterNumber = num.toString().split("").map(Number);
-  for (let i=0; i<enterNumber.length; i++)
-  { console.log(enterNumber[i])
-    if (enterNumber[i] < enterNumber[i-1])
-    {
+// this function allows for single digit numbers to considered ramp numbers
+// --------------------------------------------------------------------
+function rampNumber(num) {
+  let currentNum = num.toString().split("")//.map(Number);
+  //console.log(currentNum);
+  for (let i = 0; i < currentNum.length; i++) {
+    if (currentNum[i] < currentNum[i - 1]) {
       return false;
     }
   }
   return true;
-  
 }
 
+// // this function does NOT consider single digit numbers as ramp numbers
+// // --------------------------------------------------------------------
+// function rampNumber(num) {
+//   let currentNum = num.toString().split("").map(Number);
 
-// console.log(rampNumber(11234));
-// console.log(rampNumber(99990));
+//   for (let i = 0; i < currentNum.length; i++) {
+//     if (currentNum.length < 2) {
+//       return false;
+//     } else if (currentNum.length >= 2 && currentNum[i] < currentNum[i - 1]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// console.log(rampNumber(99999))
+
+// working function!
+// --------------------------------------------------------------------
+// function numOfRampsBelow(num) {
+//   if (num < 0 || num > 99999) {
+//     return "Invalid number!";
+//   }
+
+//   let numArr = Array.from(Array(num).keys());
+//   let counter = 0;
+//   console.log(numArr);
+// for (let i = 0; i < numArr.length; i++) {
+//   // console.log(numArr[i], rampNumber(numArr[i]))
+
+//   if (rampNumber(numArr[i])) {
+//     // console.log(numArr[i])
+//     counter++
+//   }
+// }
+// // return `${counter} total ramp numbers are less than ${num}.`;
+// console.log(`${counter} total ramp numbers are less than ${num}.`);
+//}
 
 
+
+
+// console.log(numOfRampsBelow(112))
+// console.log(numOfRampsBelow(99999))
+// console.log(rampNumber(12342))
+
+// "push yourself further" - this function prints out the found ramp numbers
+// -------------------------------------------------------------------------
 function numOfRampsBelow(num) {
-    // let numArray = Array.from(Array(num).keys());
-    let counter = 0;
-    // let bigNumArr = [];
-    let bigNum = num
-    // console.log(bigNumArr)
+  if (num < 0 || num > 99999) {
+    return "Invalid number!";
+  }
 
-    for (let i = 0; i < bigNum; i++) {
-        console.log(bigNum[i])
-        if(rampNumber(bigNum)) {
-            counter++
-        
-        }
-        bigNum = bigNum - 1;
-        // for (let j = 0; j < rampNumber[i].length; j++) {
-        //     if (rampNumber[i][j] >= rampNumber[i][j-1]) {
-        //         counter++
-        //     }
-        // }
+  let numArr = Array.from(Array(num).keys());
+  let counter = 0;
+
+  // let rampNums = [];
+
+  for (let i = 0; i < numArr.length; i++) {
+    if (rampNumber(numArr[i])) {
+      //  rampNums.push(numArr[i])
+      counter++
     }
-
-
-console.log( `${counter} total ramp numbers are less than ${num}`)
-
+  }
+  console.log(`${counter} total ramp numbers are less than ${num}.`);// The ramp numbers are: ${rampNums`;
 }
 
-
-
-
-
-
-
-
-
-
-
-
+//console.log(numOfRampsBelow(15))
 
 
 
 
 
 const getPrompt = () => {
-    rl.question('enter a number: ', (num) => {
-        numOfRampsBelow(num);
-        // getPrompt();
-      });
-  }
-  getPrompt();
+  rl.question('enter a number: ', (num) => {
+    numOfRampsBelow(num);
+    // rampNumber(num);
+    // getPrompt();
+  });
+}
+getPrompt();
